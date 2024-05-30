a26562a24d3c3033bc963f915bb75603955184e3
Jenaya F <145370280+Shava-mp4@users.noreply.github.com>
2024-05-29 18:43:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ function markQuiz() {
   if (q1Response2 == "makoto yuki") {
     q1Answer.innerHTML = "Correct!";
     numCorrect = numCorrect + 1;
-    q1Response.style.border = "2px solid rgb(186, 255, 181)";
-    q1Answer.style.color = "rgb(186, 255, 181)";
+    q1Response.style.border = "2px solid rgb(11, 148, 1)";
+    q1Answer.style.color = "rgb(11, 148, 1)";
   } else {
     q1Answer.innerHTML = "Wrong :(";
     q1Response.style.border = "2px solid rgb(255, 128, 128)";
@@ -46,8 +46,8 @@ function markQuiz() {
   if (q2Response2 == "keisuke hiraga") {
     q2Answer.innerHTML = "Correct!";
     numCorrect = numCorrect + 1;
-    q2Response.style.border = "2px solid rgb(186, 255, 181)";
-    q2Answer.style.color = "rgb(186, 255, 181)";
+    q2Response.style.border = "2px solid rgb(11, 148, 1)";
+    q2Answer.style.color = "rgb(11, 148, 1)";
   } else {
     q2Answer.innerHTML = "Wrong :(";
     q2Response.style.border = "2px solid rgb(255, 128, 128)";
@@ -57,8 +57,8 @@ function markQuiz() {
   if (q3Response2 === "gun") {
     q3Answer.innerHTML = "Correct!";
     numCorrect = numCorrect + 1;
-    q3Response.style.border = "2px solid rgb(186, 255, 181)";
-    q3Answer.style.color = "rgb(186, 255, 181)";
+    q3Response.style.border = "2px solid rgb(11, 148, 1)";
+    q3Answer.style.color = "rgb(11, 148, 1)";
   } else {
     q3Answer.innerHTML = "Wrong :(";
     q3Response.style.border = "2px solid rgb(255, 128, 128)";
@@ -68,8 +68,8 @@ function markQuiz() {
   if (q4Response2 == "megami tensei") {
     q4Answer.innerHTML = "Correct!";
     numCorrect = numCorrect + 1;
-    q4Answer.style.color = "rgb(186, 255, 181)";
-    q4Response.style.border = "2px solid rgb(186, 255, 181)";
+    q4Answer.style.color = "rgb(11, 148, 1)";
+    q4Response.style.border = "2px solid rgb(11, 148, 1)";
   } else {
     q4Answer.innerHTML = "Wrong :(";
     q4Response.style.border = "2px solid rgb(255, 128, 128)";
@@ -79,8 +79,8 @@ function markQuiz() {
   if (q5Response2 == "22") {
     q5Answer.innerHTML = "Correct!";
     numCorrect = numCorrect + 1;
-    q5Answer.style.color = "rgb(186, 255, 181)";
-    q5Response.style.border = "2px solid rgb(186, 255, 181)";
+    q5Answer.style.color = "rgb(11, 148, 1)";
+    q5Response.style.border = "2px solid rgb(11, 148, 1)";
   } else {
     q5Answer.innerHTML = "Wrong :(";
     q5Response.style.border = "2px solid rgb(255, 128, 128)";
@@ -90,8 +90,8 @@ function markQuiz() {
   if (q6Response2 === "FMFL/KNK") {
     q6Answer.innerHTML = "Correct!";
     numCorrect = numCorrect + 1;
-    q6Response.style.border = "2px solid rgb(186, 255, 181)";
-    q6Answer.style.color = "rgb(186, 255, 181)";
+    q6Response.style.border = "2px solid rgb(11, 148, 1)";
+    q6Answer.style.color = "rgb(11, 148, 1)";
   } else {
     q6Answer.innerHTML = "Wrong :(";
     q6Response.style.border = "2px solid rgb(255, 128, 128)";
